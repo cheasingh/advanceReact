7d3914c6c2a5b7b9c6a3c99eaef4eedac2cbeddd
Joe Chea <cheasingh168@gmail.com>
2020-02-05 00:17:28 +0700

    initial upload project

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+function Home() {
+    return (
+        <div>
+            <p>Hey!</p>
+            <Link href="/sell">
+                <a>Sell</a>
+            </Link>
+        </div>
+    );
+}
+
+export default Home;
---
+++ sick-fits/frontend/pages/product.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+function Product() {
+    return (
+        <div>
+            <Link href="/">
+                <a href="">to home</a>
+            </Link>
+
+        </div>
+    )
+}
+
+export default Product;
