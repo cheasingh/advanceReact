c03c12c15561919f135bdc614646d2585c7842f0
Joe Chea <cheasingh168@gmail.com>
2020-06-01 08:06:34 +0700

    ready for paging

--- sick-fits/frontend/components/Item.js
+++ sick-fits/frontend/components/Item.js
@@ -4,6 +4,7 @@ import Link from "next/link";
 import Title from "./styles/Title";
 import ItemStyles from "./styles/ItemStyles";
 import PriceTag from "./styles/PriceTag";
+import DeleteItem from "./DeleteItem";
 import formatMoney from "../lib/formatMoney";
 
 class Item extends Component {
@@ -39,7 +40,7 @@ class Item extends Component {
             <a>Edit</a>
           </Link>
           <button>Add to Cart</button>
-          <button>Delete</button>
+          <DeleteItem id={item.id}>Delete</DeleteItem>
         </div>
       </ItemStyles>
     );
